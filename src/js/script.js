46ff1fbb63b8b345e86fbb810f26603cbafb85a4
mathias <mathiasmichaelweiss@gmail.com>
2021-03-19 16:59:31 +0100

    added help message to project

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,18 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
-  /* document.querySelector(".container").style.paddingBottom = `${
-  document.querySelector(".header").clientHeight + "px"
-}`; */
-
-  /* inputs for Modal */
-
+  /* inputs for Modal and JS mini DB*/
   const modalInputs = {
     upName:
-      '<div class="reg-form-item"><p class="reg-form-item_title">name</p><p><input class="input-name upName" type="name" placeholder="enter your name" maxlength="20"/></p><div class="line"></div></div>',
+      '<div class="reg-form-item"><p class="reg-form-item_title">name</p><p class="upNameContainer"><input class="input-name upName" type="name" placeholder="enter your name" maxlength="20"/></p><div class="line"></div></div>',
     inName:
-      '<div class="reg-form-item"><p class="reg-form-item_title">name</p><p><input class="input-name inName" type="name" placeholder="enter your name" maxlength="20"/></p><div class="line"></div></div>',
+      '<div class="reg-form-item"><p class="reg-form-item_title">name</p><p class="inNameContainer"><input class="input-name inName" type="name" placeholder="enter your name" maxlength="20"/></p><div class="line"></div></div>',
     email:
       '<div class="reg-form-item"><p class="reg-form-item_title">e-mail</p><p><input class="input-email" type="email" placeholder="enter your e-mail" /></p><div class="line"></div></div>',
     upPassword:
-      '<div class="reg-form-item"><p class="reg-form-item_title">password</p><p class="password"><input class="input-password upPassword" type="password" placeholder="enter your password"  maxlength="25"/><span class="vision-container"></span><div class="line"></div></p></div>',
+      '<div class="reg-form-item"><p class="reg-form-item_title">password</p><p class="password upPasswordContainer"><input class="input-password upPassword" type="password" placeholder="enter your password"  maxlength="25"/><span class="vision-container"></span><div class="line"></div></p></div>',
     inPassword:
-      '<div class="reg-form-item"><p class="reg-form-item_title">password</p><p class="password"><input class="input-password inPassword" type="password" placeholder="enter your password"  maxlength="25"/><span class="vision-container"></span><div class="line"></div></p></div>',
+      '<div class="reg-form-item"><p class="reg-form-item_title">password</p><p class="password inPasswordContainer"><input class="input-password inPassword" type="password" placeholder="enter your password"  maxlength="25"/><span class="vision-container"></span><div class="line"></div></p></div>',
     checkbox:
       '<input type="checkbox" class="privacy-check" /><p class="privacy">i agree with all <a href="">terms & conditions & privacy policy</a></p>'
   };
@@ -47,6 +42,13 @@ window.addEventListener("DOMContentLoaded", () => {
       "upClass",
       ".container"
     ]
+  };
+
+  const svgDB = {
+    help:
+      '<svg width="12" height="12" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="10" height="10" rx="5" fill="#E5E5E5"/><path d="M4.39453 6.39844C4.39974 6.08854 4.4349 5.84375 4.5 5.66406C4.5651 5.48438 4.69792 5.28516 4.89844 5.06641L5.41016 4.53906C5.62891 4.29167 5.73828 4.02604 5.73828 3.74219C5.73828 3.46875 5.66667 3.25521 5.52344 3.10156C5.38021 2.94531 5.17188 2.86719 4.89844 2.86719C4.63281 2.86719 4.41927 2.9375 4.25781 3.07812C4.09635 3.21875 4.01562 3.40755 4.01562 3.64453H3.29297C3.29818 3.22266 3.44792 2.88281 3.74219 2.625C4.03906 2.36458 4.42448 2.23438 4.89844 2.23438C5.39062 2.23438 5.77344 2.36719 6.04688 2.63281C6.32292 2.89583 6.46094 3.25781 6.46094 3.71875C6.46094 4.17448 6.25 4.6237 5.82812 5.06641L5.40234 5.48828C5.21224 5.69922 5.11719 6.0026 5.11719 6.39844H4.39453ZM4.36328 7.63672C4.36328 7.51953 4.39844 7.42188 4.46875 7.34375C4.54167 7.26302 4.64844 7.22266 4.78906 7.22266C4.92969 7.22266 5.03646 7.26302 5.10938 7.34375C5.18229 7.42188 5.21875 7.51953 5.21875 7.63672C5.21875 7.75391 5.18229 7.85156 5.10938 7.92969C5.03646 8.00521 4.92969 8.04297 4.78906 8.04297C4.64844 8.04297 4.54167 8.00521 4.46875 7.92969C4.39844 7.85156 4.36328 7.75391 4.36328 7.63672Z" fill="rgba(0, 0, 0, 0.8)"/></svg>',
+    helpError:
+      '<svg width="12" height="12" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="10" height="10" rx="5" fill="#FF7676"/><path d="M4.39453 6.39844C4.39974 6.08854 4.4349 5.84375 4.5 5.66406C4.5651 5.48438 4.69792 5.28516 4.89844 5.06641L5.41016 4.53906C5.62891 4.29167 5.73828 4.02604 5.73828 3.74219C5.73828 3.46875 5.66667 3.25521 5.52344 3.10156C5.38021 2.94531 5.17188 2.86719 4.89844 2.86719C4.63281 2.86719 4.41927 2.9375 4.25781 3.07812C4.09635 3.21875 4.01562 3.40755 4.01562 3.64453H3.29297C3.29818 3.22266 3.44792 2.88281 3.74219 2.625C4.03906 2.36458 4.42448 2.23438 4.89844 2.23438C5.39062 2.23438 5.77344 2.36719 6.04688 2.63281C6.32292 2.89583 6.46094 3.25781 6.46094 3.71875C6.46094 4.17448 6.25 4.6237 5.82812 5.06641L5.40234 5.48828C5.21224 5.69922 5.11719 6.0026 5.11719 6.39844H4.39453ZM4.36328 7.63672C4.36328 7.51953 4.39844 7.42188 4.46875 7.34375C4.54167 7.26302 4.64844 7.22266 4.78906 7.22266C4.92969 7.22266 5.03646 7.26302 5.10938 7.34375C5.18229 7.42188 5.21875 7.51953 5.21875 7.63672C5.21875 7.75391 5.18229 7.85156 5.10938 7.92969C5.03646 8.00521 4.92969 8.04297 4.78906 8.04297C4.64844 8.04297 4.54167 8.00521 4.46875 7.92969C4.39844 7.85156 4.36328 7.75391 4.36328 7.63672Z" fill="white"/></svg>'
   };
 
   class Modal {
@@ -122,7 +124,12 @@ window.addEventListener("DOMContentLoaded", () => {
           ${this.email}
           ${this.password}
           ${this.checkbox}
-            <button>${this.btnText}</button>
+          <div class="btn-container">
+          <button>${this.btnText}</button>
+          <div class="help">
+            
+          </div>
+          </div>
           </form>
           <div class="go-to-signin">
             already have an account? <a href="#" class="toggle-window ${this.toggleClass}">${this.bottomLink}</a>
@@ -142,42 +149,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const modal = document.querySelectorAll(".modal");
   const toggleWindowBtn = document.querySelectorAll(".toggle-window");
 
-  /* function createWindow(willRemove, willCreate) {
-  document.querySelector(willRemove).remove();
-  new Modal(willCreate).render();
-} */
-
-  /*   if (inBtn === null) {
-    upBtn.addEventListener("click", e => {
-      document.querySelector(".upClass").remove();
-      new Modal(...signUp).render();
-      console.log(inBtn);
-      return inBtn;
+  // toggle modal window
+  function toggleWindow(i) {
+    modal.forEach(item => {
+      item.classList.remove("hideWindow");
+      modal[i].classList.add("hideWindow");
+      item.style = "";
+      document.querySelector("body").style = "";
+      document.querySelector(".header").style =
+        "transition: color 1s; width: 100%; display: flex; flex-wrap: wrap;  justify-content: center; font-size: 2rem; opacity: 0.4; margin-bottom: 2rem;";
+      help[i].innerHTML = svgDB.help;
     });
   }
 
-  if (upBtn === null) {
-    inBtn.addEventListener("click", e => {
-      document.querySelector(".inClass").remove();
-      new Modal(...signIn).render();
-      console.log(upBtn);
-      return upBtn;
-    });
-  } */
-
-  // toggle modal window
   toggleWindowBtn.forEach((btn, i) => {
     modal[0].classList.add("hideWindow");
-    btn.addEventListener("click", () => {
-      modal.forEach((item, j) => {
-        item.classList.remove("hideWindow");
-        modal[i].classList.add("hideWindow");
-        item.style = "";
-        document.querySelector("body").style = "";
-        document.querySelector(".header").style =
-          "transition: color 1s; width: 100%; display: flex; flex-wrap: wrap;  justify-content: center; font-size: 2rem; opacity: 0.4; margin-bottom: 2rem;";
-      });
-    });
+    btn.addEventListener("click", () => toggleWindow(i));
   });
 
   // toggle password vision
@@ -226,7 +213,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const errors = {
     incorrectName: "incorrect name: please use only numbers and letters",
     incorrectPassword:
-      "incorrect password: password must be at least 8 characters long"
+      "incorrect password: password must be at least 8 characters long",
+    whatCanIUse:
+      "please, use only numbers and letters. password must be at least 8 charactes long. name must be a least 4 characters long."
   };
 
   const formItem = document.querySelectorAll(".reg-form");
@@ -236,12 +225,52 @@ window.addEventListener("DOMContentLoaded", () => {
   const inputUpPassword = document.querySelector(".upPassword");
   const inputEmail = document.querySelector(".input-email");
   const header = document.querySelector(".header");
+  const inNameContainer = document.querySelector(".inNameContainer");
+  const upNameContainer = document.querySelector(".upNameContainer");
+  const inPasswordContainer = document.querySelector(".inPasswordContainer");
+  const upPasswordContainer = document.querySelector(".upPasswordContainer");
 
-  function validation(input) {
+  // Help btn
+  const help = document.querySelectorAll(".help");
+  const submitBtn = document.querySelectorAll("button");
+
+  function showHelpMessage(message) {
+    const helpMessage = document.createElement("div");
+    helpMessage.classList.add("message");
+    helpMessage.style =
+      "position: absolute; width: 190px; font-size: 10px; background: rgba(255, 255, 255, 0.95); bottom: 0; font-variant: normal; border: 1px solid #B2B2B2; padding: 0.5rem; animation: helpMessage 1s; line-height: 1rem";
+    helpMessage.innerHTML = `
+    ${message}
+    `;
+    const helpMessage2 = document.createElement("div");
+    helpMessage2.classList.add("message");
+    helpMessage2.style =
+      "position: absolute; width: 190px; font-size: 10px; background: rgba(255, 255, 255, 0.95); bottom: 0; font-variant: normal; border: 1px solid #B2B2B2; padding: 0.5rem; animation: helpMessage 1s; line-height: 1rem";
+    helpMessage2.innerHTML = `
+    ${message}
+    `;
+    /* formItem.forEach(item => item.append(helpMessage)); */
+    formItem[0].append(helpMessage);
+    formItem[1].append(helpMessage2);
+  }
+
+  function hideHelpMessage() {
+    document.querySelectorAll(".message").forEach(item => item.remove());
+  }
+
+  help.forEach(item => {
+    item.innerHTML = svgDB.help;
+    item.addEventListener("mouseover", e =>
+      showHelpMessage(errors.whatCanIUse)
+    );
+    item.addEventListener("mouseout", e => hideHelpMessage());
+  });
+
+  function validation(input, container) {
     const myRegexp = /\W/g;
     const bgColor = document.querySelector("body");
 
-    modal.forEach(item => {
+    modal.forEach((item, i) => {
       if (input.classList.contains("input-name")) {
         if (
           input.value === "" ||
@@ -250,18 +279,19 @@ window.addEventListener("DOMContentLoaded", () => {
         ) {
           item.style =
             "border: 1px solid rgba(255, 0, 0, 0.5); box-sizing: border-box; filter: drop-shadow(0px 0px 3px rgba(255, 0, 0, 0.25)); animation: error 0.1s";
-          input.style =
-            "transition: padding-left 1s; border: 1px solid rgba(255, 0, 0, 0.5); box-sizing: border-box; filter: drop-shadow(0px 0px 3px rgba(255, 0, 0, 0.25)); padding-left: 10px; animation: error 0.1s";
+          container.style =
+            "transition: padding-left 1s; border: 1px solid rgba(255, 0, 0, 0.5); box-sizing: border-box; filter: drop-shadow(0px 0px 3px rgba(255, 0, 0, 0.25)); padding-left: 10px; padding-right: 10px; animation: error 0.1s; margin-bottom: 2px";
           bgColor.style =
             "transition: background-color 3s; background-color: #FF7777;";
           header.style =
             "transition: color 1s; width: 100%; display: flex; flex-wrap: wrap;  justify-content: center; font-size: 2rem; opacity: 1; margin-bottom: 2rem; color: rgba(255, 255, 255, 1)";
-          isError = true;
+          help[i].innerHTML = svgDB.helpError;
         } else {
+          help[i].innerHTML = svgDB.help;
+          help[i].style = "";
           item.style = "";
-          input.style = "";
+          container.style = "";
           bgColor.style = "";
-          isError = false;
         }
       }
 
@@ -273,18 +303,19 @@ window.addEventListener("DOMContentLoaded", () => {
         ) {
           item.style =
             "border: 1px solid rgba(255, 0, 0, 0.5); box-sizing: border-box; filter: drop-shadow(0px 0px 3px rgba(255, 0, 0, 0.25)); animation: error 0.1s";
-          input.style =
-            "transition: padding-left 1s; border: 1px solid rgba(255, 0, 0, 0.5); box-sizing: border-box; filter: drop-shadow(0px 0px 3px rgba(255, 0, 0, 0.25)); padding-left: 10px; animation: error 0.1s";
+          container.style =
+            "transition: padding-left 1s; border: 1px solid rgba(255, 0, 0, 0.5); box-sizing: border-box; filter: drop-shadow(0px 0px 3px rgba(255, 0, 0, 0.25)); padding-left: 10px; padding-right: 10px; animation: error 0.1s; margin-bottom: 2px";
           bgColor.style =
             "transition: background-color 3s; background-color: #FF7777;";
           header.style =
             "transition: color 1s; width: 100%; display: flex; flex-wrap: wrap;  justify-content: center; font-size: 2rem; opacity: 1; margin-bottom: 2rem; color: rgba(255, 255, 255, 1)";
-          isError = true;
+          help[i].innerHTML = svgDB.helpError;
         } else {
+          help[i].innerHTML = svgDB.help;
+          help[i].style = "";
           item.style = "";
-          input.style = "";
+          container.style = "";
           bgColor.style = "";
-          isError = false;
         }
       }
 
@@ -293,44 +324,29 @@ window.addEventListener("DOMContentLoaded", () => {
           item.style =
             "border: 1px solid rgba(255, 0, 0, 0.5); box-sizing: border-box; filter: drop-shadow(0px 0px 3px rgba(255, 0, 0, 0.25)); animation: error 0.1s; animation-iteration-count: 2;";
           input.style =
-            "transition: padding-left 1s; border: 1px solid rgba(255, 0, 0, 0.5); box-sizing: border-box; filter: drop-shadow(0px 0px 3px rgba(255, 0, 0, 0.25)); padding-left: 10px; animation: error 0.1s; animation-iteration-count: 2;";
+            "transition: padding-left 1s; border: 1px solid rgba(255, 0, 0, 0.5); box-sizing: border-box; filter: drop-shadow(0px 0px 3px rgba(255, 0, 0, 0.25)); padding-left: 10px; padding-right: 10px; animation: error 0.1s; margin-bottom: 2px";
           bgColor.style =
             "transition: background-color 3s; background-color: #FF7777;";
           header.style =
             "transition: color 1s; width: 100%; display: flex; flex-wrap: wrap;  justify-content: center; font-size: 2rem; opacity: 1; margin-bottom: 2rem; color: rgba(255, 255, 255, 1)";
-          isError = true;
         } else {
           item.style = "";
           input.style = "";
           bgColor.style = "";
-          isError = false;
         }
       }
     });
   }
 
-  let isError = false;
-
-  function clearError() {
-    if (!isError) {
-      inputInName.style = "";
-      inputUpPassword.style = "";
-      inputEmail.style = "";
-    }
-  }
-
-  formItem.forEach((form, i) => {
+  formItem.forEach(form => {
     form.addEventListener("submit", e => {
-      console.log(isError);
       e.preventDefault();
-      if (
-        document.querySelectorAll(".modal")[0].classList.contains("hideWindow")
-      ) {
-        validation(inputInName);
-        validation(inputInPassword);
+      if (modal[0].classList.contains("hideWindow")) {
+        validation(inputInName, inNameContainer);
+        validation(inputInPassword, inPasswordContainer);
       } else {
-        validation(inputUpName);
-        validation(inputUpPassword);
+        validation(inputUpName, upNameContainer);
+        validation(inputUpPassword, upPasswordContainer);
         validation(inputEmail);
       }
     });
